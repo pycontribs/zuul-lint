5eb654a66af5e900007045c4d4d8c57b947ad891
Sorin Sbarnea <ssbarnea@redhat.com>
2020-01-13 09:08:09 +0000

    Enable two basic tests

--- tests/test_zuul_lint.py
+++ tests/test_zuul_lint.py
@@ -1,2 +1,19 @@
-def test_foo():
-    pass
+import pytest
+import sh
+
+
+def test_invalid():
+    try:
+        sh.python(["-m", "zuul_lint", "tests/data/zuul-config-invalid.yaml"])
+    except sh.ErrorReturnCode_1:
+        return
+    except sh.ErrorReturnCode as e:
+        pytest.fail(e)
+    pytest.fail("Expected to fail")
+
+
+def test_valid():
+    try:
+        sh.python(["-m", "zuul_lint", "tests/data/zuul-config-valid.yaml"])
+    except sh.ErrorReturnCode as e:
+        pytest.fail(e)
