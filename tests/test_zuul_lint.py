f70e0f72f1d3760b708049e54448724047e778f8
Pedro Baptista <32106544+pmpbaptista@users.noreply.github.com>
2023-10-15 17:57:04 +0000

    feat: update project
    Project update to use ruff pre-commit hook
    Add requirements.txt with pinned and bumped versions
    Update tox.ini
    Replace os.path with pathlib
    Replace shell with subprocess in tests
    
    Issue: none

--- tests/test_zuul_lint.py
+++ tests/test_zuul_lint.py
@@ -1,19 +1,35 @@
+"""Zuul Linter Tests."""
+import subprocess
+
 import pytest
-import sh
 
 
 def test_invalid():
+    """Test that the linter correctly detects errors in an invalid Zuul YAML file.
+
+    Raises
+    ------
+        pytest.fail: If the linter does not fail as expected.
+    """
     try:
-        sh.python(["-m", "zuul_lint", "tests/data/zuul-config-invalid.yaml"])
-    except sh.ErrorReturnCode_1:
-        return
-    except sh.ErrorReturnCode as e:
+        subprocess.check_call(["python", "-m", "zuul_lint",
+                               "tests/data/zuul-config-invalid.yaml"])
+    except subprocess.CalledProcessError as e:
+        if e.returncode == 1:
+            return
         pytest.fail(e)
     pytest.fail("Expected to fail")
 
 
 def test_valid():
+    """Test that the linter does not detect errors in a valid Zuul YAML file.
+
+    Raises
+    ------
+    pytest.fail: If the linter fails unexpectedly.
+    """
     try:
-        sh.python(["-m", "zuul_lint", "tests/data/zuul-config-valid.yaml"])
-    except sh.ErrorReturnCode as e:
+        subprocess.check_call(["python", "-m", "zuul_lint",
+                               "tests/data/zuul-config-valid.yaml"])
+    except subprocess.CalledProcessError as e:
         pytest.fail(e)
